f6369dbe0cd174cbaddbc531e0d5e6738dcb68d7
koenigbrok <86680868+koenigbrok@users.noreply.github.com>
2021-12-27 22:12:00 +0000

    addMessage POST API added

--- src/main/java/org/koenigbrok/vic/projectRest/resources/messageTestResource.java
+++ src/main/java/org/koenigbrok/vic/projectRest/resources/messageTestResource.java
@@ -2,7 +2,9 @@ package org.koenigbrok.vic.projectRest.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,16 +19,26 @@ public class messageTestResource {
 	messageService ms = new messageService();
 	
 	 @GET 
-	 @Produces(MediaType.APPLICATION_XML)
+	 @Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessage() {
 		return  ms.getAllMessages();
 	}
 	 
+	 @POST
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 @Produces(MediaType.APPLICATION_JSON)
+		public Message  addMessage(Message message) {
+		return ms.addMessage(message);
+		}
+	 
+	 
 	 @GET
 	 @Path("/{messageId}")
-	 @Produces(MediaType.APPLICATION_XML)
+	 @Produces(MediaType.APPLICATION_JSON)
 	 public Message getMessage(@PathParam("messageId")  long messageId) {
 		  
 		return ms.getMessage(messageId);
 	}
+	 
+	 
 }
