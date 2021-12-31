b476b74f1af9fe91da2b0135c4fdbc9356078d32
koenigbrok <86680868+koenigbrok@users.noreply.github.com>
2021-12-31 22:15:55 +0000

    Pagination based on size and year paramater functionality

--- src/main/java/org/koenigbrok/vic/projectRest/messanger/service/messageService.java
+++ src/main/java/org/koenigbrok/vic/projectRest/messanger/service/messageService.java
@@ -1,6 +1,7 @@
 package org.koenigbrok.vic.projectRest.messanger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +24,26 @@ public class messageService {
 	public List<Message> getAllMessages(){
 		
 		return new ArrayList<Message>(messages.values());	
+	}
+	
+	public List<Message> getAllMessagesByYear(int year){
+		List<Message> messagesForYear = new ArrayList<Message>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message : messages.values()) {
+			if((cal.get(Calendar.YEAR)== year)){
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;	
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size){
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if(start + size > list.size()) {
+			return new ArrayList<Message>();
+		}
+		
+		return list.subList(start, start + size);
 	}
 	
 	public Message getMessage(long id) {
--- src/main/java/org/koenigbrok/vic/projectRest/resources/messageTestResource.java
+++ src/main/java/org/koenigbrok/vic/projectRest/resources/messageTestResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.koenigbrok.vic.projectRest.messanger.model.Message;
@@ -22,9 +23,16 @@ public class messageTestResource {
 	
 	 @GET 
 	 @Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessage() {
+	public List<Message> getMessage(@QueryParam("year") int year, @QueryParam("start") int start, @QueryParam("size") int size) {
+		 if(year > 0) {
+			 return ms.getAllMessagesByYear(year);
+		 }
+		 if(start >= 0 && size >= 0) {
+			 return ms.getAllMessagesPaginated(start, size);
+		 }
 		return  ms.getAllMessages();
 	}
+	
 	 
 	 @POST
 	 @Consumes(MediaType.APPLICATION_JSON)
