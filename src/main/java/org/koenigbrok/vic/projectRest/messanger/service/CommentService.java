f732513624b2e9a60fa8b4b94fd721e6fd59c500
koenigbrok <86680868+koenigbrok@users.noreply.github.com>
2022-01-12 19:43:38 +0000

    WebApplicationExceptione error handling

--- src/main/java/org/koenigbrok/vic/projectRest/messanger/service/CommentService.java
+++ src/main/java/org/koenigbrok/vic/projectRest/messanger/service/CommentService.java
@@ -2,11 +2,16 @@ package org.koenigbrok.vic.projectRest.messanger.service;
 
 import java.util.*;
 
-
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.koenigbrok.vic.projectRest.messanger.database.DatabaseClass;
 import org.koenigbrok.vic.projectRest.messanger.model.Comment;
+import org.koenigbrok.vic.projectRest.messanger.model.ErrorMessage;
 import org.koenigbrok.vic.projectRest.messanger.model.Message;
+
+
 
 public class CommentService {
 
@@ -19,8 +24,24 @@ public class CommentService {
 	}
 	
 	public Comment getComment(long messageId, long commentId ) {
-		Map<Long, Comment> comments = messages.get(messageId).getComments();
-		return comments.get(commentId);
+		
+		ErrorMessage em = new ErrorMessage("fnf 404", 404, "http://org.koenigbrok.org");
+		Response resp =  Response.status(Status.NOT_FOUND)
+				.entity(em)
+				.build();
+		
+		Message msg = messages.get(messageId);
+		if(msg == null ) {
+			throw new  WebApplicationException(resp);
+		}
+		Map<Long, Comment> comments = msg.getComments();
+		Comment cmt =  comments.get(commentId);
+		if(cmt == null) {
+			throw new WebApplicationException(resp);
+		}
+		return cmt;
+		
+		
 	}
 	
 	public Comment addComment(long messageId, Comment comment ) {
